13508cab01dbafb1c4733870946eb9b5f8389aeb
shiamsharif <shiam.sharif.07@gmail.com>
2024-04-01 11:35:46 +0600

    Display all json data

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,41 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
+  const {
+    Title,
+    Cover,
+    Author_Name,
+    Author_Image,
+    Reading_Time,
+    Posted_Date,
+    Hashtags,
+  } = blog;
 
-  return <div></div>;
+  return (
+    <div>
+      <img src={Cover} alt="" />
+      <div className="flex justify-between">
+        <div className="flex ">
+          <img className="w-14" src={Author_Image} alt="" />
+          <div className="ml-6">
+            <h3 className="text-xl text-blue-500 font-bold">{Author_Name}</h3>
+            <p>{Posted_Date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{Reading_Time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{Title}</h2>
+      <p className="text-gray-700">
+        {Hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a>#{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,20 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
-const Blog = () => {
+const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     fetch("blogs.json")
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
 
   return (
     <div className="md:w-2/3">
       <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.ID} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
 
-export default Blog;
+export default Blogs;
